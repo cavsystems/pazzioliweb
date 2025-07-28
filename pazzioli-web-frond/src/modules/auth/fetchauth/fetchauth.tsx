51f63d262189e7bceeb675de9266f37be3a8cc33
cavsystems <desarrollo@cavsystmes.com.co>
2025-07-27 21:29:50 -0500

    authversion-5

--- pazzioli-web-frond/src/modules/auth/fetchauth/fetchauth.tsx
+++ pazzioli-web-frond/src/modules/auth/fetchauth/fetchauth.tsx
@@ -2,7 +2,11 @@ import api from '../../../apicofig'
 export const fetchAuth = {
     login: async (data: any) => {
         try {
-        const response = await api.post('/auth/login', data);
+        const response = await api.post('/auth/login', data,{
+        headers: {
+          'X-TenantID': data.db, // suponiendo que data.db contiene el nombre de la base de datos
+        },
+      });
         return response.data;
         } catch (error) {
         throw error;
