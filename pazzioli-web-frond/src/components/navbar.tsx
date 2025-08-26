7288926d223d7b7ff061511d46245a8a7259c977
cavsystems <desarrollo@cavsystmes.com.co>
2025-08-26 06:45:33 -0500

    commitluisdavidcastillo

---
+++ pazzioli-web-frond/src/components/navbar.tsx
@@ -0,0 +1,28 @@
+function Navbar() {
+    return ( <>
+    <nav className="navpazzioli">
+        <ul>
+
+            <li className="navizquierodsidebar">
+                <ul className="listamanenusidebar">
+                    <li className="lilogomenu">
+                        <img src="/imgs/imagenlogpa.svg" className="logonavbar"/>
+                    </li>
+                     <li className="lineaseparadora">
+
+                        
+                    </li>
+
+                       <li className="navsiguiente">
+                <img src="/imgs/imagenniconcasa.svg" className="imgnavicon"/>
+            </li>
+                </ul>
+            </li>
+           
+         
+        </ul>
+    </nav>
+    </> );
+}
+
+export default Navbar;
