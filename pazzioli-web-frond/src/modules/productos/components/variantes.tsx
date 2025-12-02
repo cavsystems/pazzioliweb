cfcc5c86700b224da7dfbf3622cf5219aee9fdaa
cavsystems <desarrollo@cavsystmes.com.co>
2025-12-02 12:58:04 -0500

    cambiosfrondvariantes

--- pazzioli-web-frond/src/modules/productos/components/variantes.tsx
+++ pazzioli-web-frond/src/modules/productos/components/variantes.tsx
@@ -16,16 +16,19 @@ stockMinimo:number;
 ubicacion?:string;
 existencias?:number;
 }
-interface Variante {
-  codigovariante:number
-  color: string;
-  talla: string;
-  material: string;
-  bodega: bodegas[];
 
+
+
+interface Variante {
+  codigovariante: number;
+  atributos: { [key: string]: string }; // <--- dinámico
+  bodega: bodegas[];
 }
 function Variantes() {
   const [variantes, setVariantes] = useState<Variante[]>([]);
+  const [atributos, setAtributos] = useState<string[]>(["Talla","Color","Material"]);
+  const [rotate4,setrotate4]=useState<boolean>(false);
+  const [atributoscelda, setAtributoscelda] = useState<string[]>([]);
  const [bodegaSeleccionada, setBodegaSeleccionada] = useState<{nombre:string;
   stockMaximo:number;
 stockMinimo:number;}[]>([]);
@@ -41,10 +44,42 @@ stockMinimo:number;}[]>([]);
           console.log(Codigobarra)
         }
        },[guardar])
+
+ useEffect(()=>{
+      
+       if(atributoscelda.length>0){
+        if (rotate4) {
+    atributoscelda.forEach((item) => {
+      const element = document.getElementById(`idretencion${item}`) as HTMLInputElement | null;
+      if (element) element.checked = true;
+    });
+  }
+       }
+      
+       },[rotate4])
+       
+       const agregarAtributoceldas = (nombre: string) => {
+  setAtributoscelda(prev => [...prev, nombre]); 
+};
+
+const agregarVariante = () => {
+  const nuevosAtributos:{ [key: string]: string } = {};
+  atributoscelda.forEach(a => {
+    nuevosAtributos[a] = "";  // cada atributo tendrá un input
+  });
+
+  setVariantes(prev => [
+    ...prev,
+    {
+      codigovariante: 0,
+      atributos: nuevosAtributos,
+      bodega: []
+    }
+  ]);
+};
+
   // Agregar nueva variante vacía
-  const agregarVariante = () => {
-    setVariantes(prev => [...prev, {codigovariante:0, color: "", talla: "", material: "", bodega: [], stockMin: 0, stockMax: 0 }]);
-  };
+ 
  const agregarbodega=(nombrebodega:{nombre: string, stockMaximo: number, stockMinimo: number,ubicacion:string,existencias:number},index:number)=>{
     let existebodega=variantes[index].bodega.find(b=>b.nombre===nombrebodega.nombre);
     if(!existebodega){
@@ -71,51 +106,85 @@ stockMinimo:number;}[]>([]);
 
   return (
     <div>
-      <h3 className="h5" style={{marginLeft:"10px",marginBottom:"12px"}}>Variantes</h3>
-      <button className="botoncontinuarguardar botonagregarvariante" onClick={agregarVariante} style={{ marginBottom: "10px" }}>
+      <div className="row" style={{padding:"12px 20px 12px 20px"}}>
+          <h3 className="h5 col-12 "  style={{marginBottom:"0px"}}>Variantes</h3>
+        <div className="col-12  col-md-8 col-sm-6 inputterceroleft inputretencion padingtopcol" >
+                       <div className="d-flex  flex-wrap flex-column " style={{position:"relative",height: "100%"}}   >
+                          <ul  className="d-flex container1  flex-wrap" >
+                         
+                        <li  style={{flex:"1",display:"flex",justifyContent:"center",gap:"12px"}}  className="classiteminput"><input style={{width:"100%"}} className="inputestilotercero " placeholder="Retenciones" /> <div ><img  src="imgs/togle.svg"  className={`${'rotate'} `} onClick={()=>{
+                          setrotate4(!rotate4)
+                        }}/></div> <div style={{alignSelf:'1'}}  className="botoncerrarall"><button className="botoncerrar botoncerrarall" type="button" ></button></div>  </li >
+                          </ul>
+
+                          { rotate4 && <div className="containeritemlivariante">
+                                            <ul className="itemcontli">
+                                             {
+                          atributos.map((item)=>{
+                        return <>
+                               <li className="licheckterceros" > <input type="checkbox" id={`idretencion${item}`} onChange={(e:React.ChangeEvent<HTMLInputElement>)=>{
+                                                                 if (e.target.checked) {
+                                     // el checkbox está seleccionado
+
+                                     setAtributoscelda((prev)=> [...prev,item])
+                                   } else {
+                                       setAtributoscelda(atributoscelda.filter(item2=> item2!==item))
+                                     // el checkbox no está seleccionado
+                                   }
+                                                           }} /> <span>{item}</span></li>
+                        </>
+                          })
+                         } 
+                                               
+                                                     
+                                                  
+                                              
+                                              
+                                         
+                                            </ul>
+                                            </div>}
+                       
+                       </div>
+                    </div>
+                    <div className="col-12  col-md-4 col-sm-6  d-flex justify-content-md-end justify-content-center padingtopcol" style={{height:"fit-content"}}>
+                       <button className="botoncontinuarguardar botonagregarvariante" onClick={agregarVariante} style={{ marginBottom: "0px" }}>
         Agregar
       </button>
-
-      <CTable>
+                    </div>
+        
+      </div>
+     
+    
+   {
+    variantes.length>0 && ( <CTable>
         <CTableHead>
           <CTableRow>
-            <CTableHeaderCell>Color</CTableHeaderCell>
-            <CTableHeaderCell>Talla</CTableHeaderCell>
-            <CTableHeaderCell>Material</CTableHeaderCell>
+            {atributoscelda.map(attr => (
+      <CTableHeaderCell key={attr}>{attr}</CTableHeaderCell>
+    ))}
+ 
        
             <CTableHeaderCell>Acciones</CTableHeaderCell>
           </CTableRow>
         </CTableHead>
         <CTableBody>
-          {variantes.map((v, i) => (
-            <CTableRow key={i}>
-              <CTableDataCell>
-                <CFormInput
-                  value={v.color || ""}
-                  onChange={e => actualizarVariante(i, "color", e.target.value)}
-                  placeholder="Color"
-                  className="inputradios"
-                />
-              </CTableDataCell>
-              <CTableDataCell>
-                <CFormInput
-                  value={v.talla || ""}
-                  onChange={e => actualizarVariante(i, "talla", e.target.value)}
-                  placeholder="Talla"
-                  className="inputradios"
-                />
-              </CTableDataCell>
-              <CTableDataCell>
-                <CFormInput
-                  value={v.material || ""}
-                  onChange={e => actualizarVariante(i, "material", e.target.value)}
-                  placeholder="Material"
-                  className="inputradios"
+            {variantes.map((v, i) => (
+    <CTableRow key={i}>
+      {atributoscelda.map(attr => (
+        <CTableDataCell key={attr}>
+          <CFormInput
+            value={v.atributos[attr] || ""}
+            onChange={e => {
+              const nuevas = [...variantes];
+              nuevas[i].atributos[attr] = e.target.value;
+              setVariantes(nuevas);
+            }}
+          />
+        </CTableDataCell>
+      ))}
 
-                />
-              </CTableDataCell>
-             
-                <CTableDataCell >
+     
+  <CTableDataCell >
                                                        <div className="d-flex flex-nowrap" style={{gap:"12px"  }} >
                                                               <div className="col-6"  style={{ maxWidth: 'fit-content' }} >
                                                                <CButton  className="buttoniconnormal" onClick={()=>{
@@ -141,10 +210,12 @@ stockMinimo:number;}[]>([]);
                                                         
                                                        </div>
                                                      </CTableDataCell>
-            </CTableRow>
-          ))}
+    </CTableRow>
+  ))}
         </CTableBody>
-      </CTable>
+      </CTable>)
+   }
+     
       <Bodegasvariantes modalbo={modalbo} setmodalbo={setmodalbo} agregarbodega={agregarbodega} BodegaSeleccionada={bodegaSeleccionada}  setBodegaSeleccionada={setBodegaSeleccionada} indexvariante={indexvariante} variantes={variantes} setvariantes={setVariantes} setindexvariante={setindexvariante}  />
     </div>
   );
