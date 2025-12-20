3ba621d581986b54f02a94cde9ef492936a9993e
PROGRAMADORCAV <pazzioli@cavsystems.com.co>
2025-12-20 17:07:08 -0500

    Cambios backm

--- pazzioli-web-backend/productosmodule/src/main/java/com/pazzioliweb/productosmodule/repositori/PreciosProductoVarianteRepository.java
+++ pazzioli-web-backend/productosmodule/src/main/java/com/pazzioliweb/productosmodule/repositori/PreciosProductoVarianteRepository.java
@@ -2,8 +2,13 @@ package com.pazzioliweb.productosmodule.repositori;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import com.pazzioliweb.productosmodule.dtos.PreciosProductoVarianteDTO;
 import com.pazzioliweb.productosmodule.entity.PreciosProductoVariante;
 
 public interface PreciosProductoVarianteRepository extends JpaRepository<PreciosProductoVariante, Long> {
@@ -17,6 +22,22 @@ public interface PreciosProductoVarianteRepository extends JpaRepository<Precios
 	        Integer precioId
 	);
 	
+	@Query(
+	  		  value = """
+	  		        SELECT
+	  		  			pp.valor,
+	  		  			pp.producto_variantes_id as productoVarianteId,
+	  		  		    pp.precios_producto_id as productoVarianteId,
+	  		  		    p.precio_id as precioId,
+	  		  		    p.descripcion
+	  		        FROM precios_producto_variante as pp
+	  		        JOIN precios p ON p.precio_id = pp.precio_id
+	  		        WHERE pp.producto_variantes_id = :varianteId
+	  		        """,
+	  		  countQuery = "SELECT COUNT(*) FROM precios_producto_variante",
+	  		  nativeQuery = true
+	  		)
+	    Page<PreciosProductoVarianteDTO> preciosPrpductoVariante(@Param("varianteId")  Integer varianteId, Pageable pageable);
 	
 
 }
--- pazzioli-web-backend/productosmodule/src/main/java/com/pazzioliweb/productosmodule/repositori/PreciosRepository.java
+++ pazzioli-web-backend/productosmodule/src/main/java/com/pazzioliweb/productosmodule/repositori/PreciosRepository.java
@@ -1,9 +1,16 @@
 package com.pazzioliweb.productosmodule.repositori;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import com.pazzioliweb.productosmodule.controller.ExistenciasBodegaDTO;
+import com.pazzioliweb.productosmodule.dtos.PreciosProductoVarianteDTO;
 import com.pazzioliweb.productosmodule.entity.Precios;
 
 public interface PreciosRepository extends JpaRepository<Precios, Integer> {
 	
+
 }
